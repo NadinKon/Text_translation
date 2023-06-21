b63fe7135aaaa09557b36a91b3cb4ef127059055
NadinKon <nadinkahf@gmail.com>
2023-06-21 14:39:03 +0300

    Added detailed comments

--- process_rpy.py
+++ process_rpy.py
@@ -3,36 +3,39 @@ import argparse
 
 
 def update_translation(file_name):
+    # Создаем словарь для хранения соответствий между русскими фразами и их переводами на английский
     translations = {}
-    x = []
     russian_text = None
-    c = []
 
+    # Открываем файл для чтения
     with open(file_name, 'r', encoding='utf-8') as f:
         lines = f.readlines()
-
-
+    # Проходимся по каждой строке в файле
     for line in lines:
+        # Ищем английский текст
         translation_match = re.search(r'"([A-Z][^"]*)"', line)
+        # Ищем русский текст
         new_russian_text = re.search(r'# *.*\.*.* "([^"]*)"', line)
-
+        # Если нашли русский текст, сохраняем его
         if new_russian_text:
             russian_text = new_russian_text.group(1)
-
+        # Если у нас есть русский текст и его перевод, добавляем их в словарь
         if russian_text and translation_match:
             translations[russian_text] = translation_match.group(1)
-            russian_text = None  # reset the Russian text
-    #print(translations)
-
-
+            russian_text = None  # сбрасываем значение русского текста после его сохранения
+    # Открываем файл для записи
     with open(file_name, 'w', encoding='utf-8') as f:
+        # Проходимся по каждой строке в файле
         for line in lines:
-            #russian_text = re.search(r'# "([^"]*)"', line)
+            # Ищем русский текст
             russian_text = re.search(r'(?<!#) [a-z]* "([А-Я][^"]*)"', line)
+            # Если нашли русский текст и он есть в словаре переводов
             if russian_text and russian_text.group(1) in translations:
+                # Заменяем русский текст на английский в этой строке
                 new_line = line.replace(russian_text.group(1), translations[russian_text.group(1)])
                 f.write(new_line)
             else:
+                # Если русского текста нет, или его перевод не найден, записываем строку без изменений
                 f.write(line)
 
 
